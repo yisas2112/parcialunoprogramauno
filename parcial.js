e4a8d955a9e72a2a51e14fc937590787be2f2593
yisas2112 <rodriguezjesu1992@gmail.com>
2021-10-19 21:12:59 -0300

    Primer Commit del Primer Parcial Programación 1

---
+++ parcial.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/*
+ * APELLIDO, NOMBRE | APELLIDO, NOMBRE
+ */
+
+// Ejemplo de la estructura de un disco:
+// let disco = {
+//     Nombre: 'El lado oscuro de la Programación',
+//     Autor: 'Los Programadores Anónimos',
+//     Codigo: 1,
+//     Pistas: [
+//         {
+//             Nombre: 'Esa cajita loca llamada variablecita',
+//             Duracion: 200,
+//         },
+//         {
+//             Nombre: 'Nunca quise ser un NaN',
+//             Duracion: 180,
+//         },
+//         {
+//             Nombre: 'No quiero programar',
+//             Duracion: 90,
+//         },
+//         {
+//             Nombre: 'Bajo presión',
+//             Duracion: 240,
+//         },
+//         {
+//             Nombre: 'La odisea de las variables privadas',
+//             Duracion: 120,
+//         },
+//         {
+//             Nombre: 'Sr. Programador',
+//             Duracion: 720,
+//         },
+//     ],
+// };
+
+class Discos {
+    constructor(id,disco,autor,duracion){
+        this.id = id;
+        this.disco = disco;
+        this.autor = autor;
+        this.duracion = duracion;
+    }
+
+    
+}
+
+// Discos:
+let discos = [];
+
+// Función Cargar:
+const Cargar = () => {
+    // Cositas:
+    let nombre;    
+    let autor;
+    let song = []
+    let canciones=[{
+        nombre: '',
+        duracion:''
+    }]
+    do{        
+        nombre = prompt('Ingrese Nombre de Disco')        
+        autor = prompt('Ingrese nombre del Autor')               
+        
+    }while(!isNaN(nombre) && !isNaN(autor))
+
+    do{
+        canciones.nombre = prompt('Ingrese nombre de canción');
+        canciones.duracion = prompt('Ingrese Duración');
+        song.push(canciones)
+        let seguir = confirm('Desea seguir ingresando canciones al disco?')
+        console.log(song)
+        
+    }while(isNaN(canciones.nombre) && !isNaN(canciones.duracion) && seguir == true)
+
+
+    
+};
+
+// Función Mostrar:
+const Mostrar = () => {
+    // Variable para ir armando la cadena:
+    let html = '';
+
+    // Cositas:
+
+    // Si modificaste el nombre de la variable para ir armando la cadena, también hacelo acá:
+    document.getElementById('info').innerHTML = html; // <--- ahí es acá
+};
+
+// Todas las funciones que necesites:
